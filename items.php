8c525cfb30430470bdf9c93e5edbdac499387d3f
arshedgithub <arshedahmedmail@gmail.com>
2023-10-21 03:16:13 +0530

    add search bar

--- items.php
+++ items.php
@@ -17,17 +17,38 @@
                 font-weight: bold;
                 font-size: 20px;
             }
-
+            form {
+                text-align:center;
+                margin: 15px;
+            }
+            #result {
+                font-size: 20px;
+                color: #a0a0a0;
+                text-align:center;
+                margin: 60px;
+            }
         </style>
     </head>
     <body>
+        <form action="products.php" method="post">
+            <input type="text" name="search">
+            <input type="submit" value="search">
+        </form>
+        
         <?php 
+            // $items = array();
+            if(isset($_POST['search'])){
+                $searchKey = $_POST['search'];
+                $sql = "SELECT product_name, price, in_stock FROM products WHERE product_name LIKE '%".$_POST['search']."%'";
+            } else {
+                $sql = "SELECT product_name, price, in_stock FROM products";
+            }
 
-            $sql = "SELECT product_name, price FROM products";
             $result = $conn->query($sql); 
 
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
+                    // $items[]
                     echo "<div class='card'>";
                     echo "<img src='' alt='item_img'><br>";
                     // retrieve name from database 
@@ -41,7 +62,9 @@
                     }
                     echo "</div>";
                 }
-            } 
+            } else {
+                echo "<p id='result'>0 results<p>";
+            }
         ?>
 
         <!-- <div class="card">
