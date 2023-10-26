10a4c1e0544716cdb5130ec4f5318b1f330677d1
Rimzath <rimzathahamed21661@gmail.com>
2023-10-26 12:00:31 +0530

    message

--- login.php
+++ login.php
@@ -2,30 +2,25 @@
 <html lang="en">
 <head>
     <title>Log in</title>
-    <style>
-        form {
-            margin: auto;
-            margin-top: 50px;
-            border: 1px solid black;
-            border-radius: 5px;
-            padding: 25px;
-            width: 400px;
-        }
-    </style>
+    <link rel="stylesheet" href="css/login.css">
+    
 </head>
-<body>
-    <?php include 'menu.php' ?>
-    <form action="server.php" method="get">
-        <label for="name">Name : </label>
-        <input type="text" name="name" value=<?php if(isset($_COOKIE['username'])) echo $_COOKIE['username'] ?>>
-        <br><br>
-        <label for="pwd">Password : </label>
-        <input type="password" name="pwd" value=<?php if(isset($_COOKIE['password'])) echo $_COOKIE['password'] ?>>
-        <br><br>
-        <input type="checkbox" name="check">Remember me
-        <br><br>
-
-        <input type="submit" value="Submit">
-    </form>
+<body background="images/login1.jpg">
+<img src="images/logo3.jpg" alt="logo" class="login-logo">
+    <div class="login-form ">
+        <h1 >LOGIN</h1>
+        
+        <form action="server.php" method="get">
+            <label for="name"> Name : </label>
+            <input type="text" name="name" placeholder="Enter Your Username" value=<?php if(isset($_COOKIE['username'])) echo $_COOKIE['username'] ?>>
+            <br>
+            <label for="pwd" >Password : </label>
+            <input type="password" name="pwd" placeholder="Enter Your Password"value=<?php if(isset($_COOKIE['password'])) echo $_COOKIE['password'] ?>>
+            <br>
+            <input type="checkbox" name="check" >Remember me 
+            
+            <input type="submit" value="Submit" class="btn-login" >
+        </form>
+    </div>
 </body>
 </html>
