e1a5060b2bd3846f55f8e9417396302ce6b9a6b1
aathif2001 <aathifahamed2372@gmail.com>
2023-10-27 01:31:27 +0530

    Still making the edit button

--- admin/edit_product.php
+++ admin/edit_product.php
@@ -1,3 +1,20 @@
+<html>
+    <head>
+        <title>Edit Product Page</title>
+    </head>
+    <body>
 <?php
     
+    include '../conn.php';
+
+    $productid = $_POST['productid'];
+
+    $sql1 = "SELECT FROM products WHERE product_id = $productid" ;
+    $result = $conn->query($sql1);
+    
+    $row1 = $result->fetch_assoc();
+    echo 'Now the preset values are product id: '.$row['product_id'].' product name :'.$row['product_name'];
+
 ?>
+    </body>
+</html>
