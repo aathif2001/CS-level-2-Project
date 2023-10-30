db4905b0931f458152c6ac3e280708ff71583d6e
username here <arshedahmedmail@gmail.com>
2023-10-30 21:10:52 +0530

    complete all cart functionalities

--- cart.php
+++ cart.php
@@ -1,13 +1,24 @@
 <html>
 <head>
     <title>Cart</title>
+    <style>
+        table {
+        margin-top: 15px;
+        width: 100%;
+        }
+        table tr:nth-child(even) {
+        background-color: #f2f2f2;
+        }
+        table td {
+        padding: 10px;
+        text-align: center;
+        }
+    </style>
 </head>
 
 <body>
     <h1>Cart</h1>
-    <!-- should store in cookies  -->
-    <p>test commit from aathif</p>
-
+    
     <table>
         <tr>
             <th>Product Id</th>
@@ -19,10 +30,12 @@
 <?php 
 
     include 'conn.php';
-
-    $cart_items = array();
-    $encode_cookie = "store_prodct_".$_POST['productid'];
-    setcookie( $encode_cookie, 1);
+    
+    $total = 0;
+    if (isset($_POST['productid'])){
+        $encode_cookie = "store_prodct_".$_POST['productid'];
+        setcookie( $encode_cookie, 1);
+    }
 
     foreach ($_COOKIE as $key => $el) {
         if (substr($key,0,13) == "store_prodct_") {
@@ -31,15 +44,22 @@
             $sql = "SELECT product_name, price FROM products WHERE product_id='$product_id'";
             $result = $conn->query($sql);
             $row = $result->fetch_assoc();
-            echo "<tr><td>".$product_id."</td><td>".$row['product_name']."</td><td>".$el."</td><td>".$row['price']."</td></tr>";
+
+            if($row != NULL){
+                
+                global $total;
+                $total += $row['price'];
+                
+                echo "<tr><td>".$product_id."</td><td>".$row['product_name']."</td><td>".$el."</td><td>".$row['price']."</td></tr>";
+            }
         }
     }
 ?>
         <tr>
             <td></td>
             <td></td>
-            <td></td>
-            <td></td>
+            <td><b>Total : </b></td>
+            <td><?php echo $total; ?></td>
         </tr>
     </table>
 
